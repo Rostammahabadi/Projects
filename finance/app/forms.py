f1e17daade2d7b72d6218104be105fde9f97f382
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-12 17:44:09 -0600

    deleted file

--- finance/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtform import StringField, SubmitField
-
-
-class StockForm(FlaskForm):
-    ticker = StringField('Ticker')
-    #validators=[DataRequired()]
-    submit = SubmitField('Search Stock')
